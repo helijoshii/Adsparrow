eb5c07e1a3e6b49e159d8a238054dfa91e6a2d8f
@heli.webapprise <heli.webapprise@gmail.com>
2024-09-12 15:26:38 +0530

    fixed path

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,9 @@ import Analytics from './pages/Admin/Analytics';
 import ManageAdAccount from './pages/Admin/Setting/ManageAdAccount';
 import ManageGoogleAccount from './pages/Admin/Setting/ManageGoogleAccount';
 import MasterAdmin from './pages/MasterAdmin/MasterAdmin';
-import MasterDashboard from './pages/MasterAdmin/masterDashboard';
 import ManageUser from './pages/MasterAdmin/ManageUser';
 import Demo from './pages/Demo';
+import MasterDashboard1 from './pages/MasterAdmin/MasterDashboard1';
 
 
 function App() {
@@ -21,7 +21,6 @@ function App() {
     <div className='wrapper'>
       <Routes>
         <Route path="/login" element={<Login />} />
-        {/* <Route path="/dashboard" element={<Dashboard />} /> */}
         <Route path="/" element={<MainLayout />}>
           <Route index element={<Dashboard />} />
           <Route path="analytics" element={<Analytics />} />
@@ -30,7 +29,7 @@ function App() {
           <Route path="managegoogleaccount" element={<ManageGoogleAccount />} />
         </Route>
         <Route path="/MasterAdmin" element={<MasterAdmin />}>
-          <Route index element={<MasterDashboard />} />
+          <Route index element={<MasterDashboard1 />} />
           <Route path="ManageUser" element={<ManageUser />} />
         </Route>
       </Routes>
---
+++ src/pages/MasterAdmin/MasterDashboard1.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUsers, faRectangleAd  
+} from "@fortawesome/free-solid-svg-icons";
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+
+const MasterDashboard1 = () => {
+    return (
+        <div>
+          <div className="container-fluid">
+            <div class="row pt-3">
+              <div class="col-auto">
+                <h2 class="main_title">Dashboard</h2>
+              </div>
+            </div>
+            <div class="row pt-2">
+              <div class="col-lg-auto col-md-auto col-sm-auto col-12">
+                <div class="white_shadow dasboard-3">
+                  <div class="left">
+                    <span>Total User Counts</span>
+                    <h3>11 </h3>
+                  </div>
+                  <div class="right">
+                    <span>
+                    <FontAwesomeIcon icon={faUsers} />
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-auto col-md-auto col-sm-auto col-12">
+                <div class="white_shadow dasboard-3">
+                  <div class="left">
+                    <span>Total META ADS Counts</span>
+                    <h3>30 </h3>
+                  </div>
+                  <div class="right">
+                    <span>
+                    <FontAwesomeIcon icon={faRectangleAd} />
+                    </span>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-auto col-md-auto col-sm-auto col-12">
+                <div class="white_shadow dasboard-3">
+                  <div class="left">
+                    <span>Total Google Counts</span>
+                    <h3>1 </h3>
+                  </div>
+                  <div class="right">
+                    <span>
+                    <FontAwesomeIcon icon={faGoogle} />
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )
+}
+
+export default MasterDashboard1
