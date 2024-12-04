3b7b7361ed4f59ca75d825513e37eaf716766bc4
heli.webapprise <heli.webapprise@gmail.com>
2024-12-04 18:00:35 +0530

    feat: Email initial in account profile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,77 @@
+// import { useNavigate } from "react-router-dom";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faBars } from "@fortawesome/free-solid-svg-icons";
+// import axios from "axios";
+
+// const Navbar = () => {
+//   const navigate = useNavigate();
+//   const logout = async () => {
+//     const API_BASE_URL = import.meta.env.VITE_API_BASE_URL; // Access the env variable
+//     const res = await axios.post(
+//       `${API_BASE_URL}/logout/`
+//     );
+//     if (res.status === 200) {
+//       localStorage.removeItem("token");
+//       navigate("/login");
+//       // console.log("Logged out");
+//     }
+//   };
+
+//   return (
+//     <>
+//       <div className="app-header">
+//         <nav className="navbar navbar-expand-lg navbar-light">
+//           {/* <ul className="navbar-nav">
+//             <li className="nav-item d-block d-xl-none">
+//               <a
+//                 className="nav-link sidebartoggler nav-icon-hover"
+//                 id="headerCollapse"
+//                 href="#"
+//               >
+//                 <FontAwesomeIcon icon={faBars} />
+//               </a>
+//             </li>
+//           </ul> */}
+//           <div
+//             className="navbar-collapse justify-content-end px-0"
+//             id="navbarNav"
+//           >
+//             <ul className="navbar-nav flex-row ms-auto align-items-center justify-content-end">
+//               <li className="nav-item dropdown">
+//                 <a
+//                   className="nav-link nav-icon-hover"
+//                   href="#"
+//                   id="drop2"
+//                   data-bs-toggle="dropdown"
+//                   aria-expanded="false"
+//                 >
+//                   SR
+//                 </a>
+//                 <div
+//                   className="dropdown-menu dropdown-menu-end dropdown-menu-animate-up"
+//                   aria-labelledby="drop2"
+//                 >
+//                   <div className="message-body">
+//                     <p
+//                       className="btn btn-outline-primary mx-3 mt-2 d-block"
+//                       onClick={logout}
+//                     >
+//                       Logout
+//                     </p>
+//                   </div>
+//                 </div>
+//               </li>
+//             </ul>
+//           </div>
+//         </nav>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Navbar;
+
+
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
@@ -5,15 +79,17 @@ import axios from "axios";
 
 const Navbar = () => {
   const navigate = useNavigate();
+  // const userData = JSON.parse(localStorage.getItem("user_info"));
+  const userData = JSON.parse(localStorage.getItem("user_info")) || {};
+  const userEmail = userData.email || "";
   const logout = async () => {
-    const API_BASE_URL = import.meta.env.VITE_API_BASE_URL; // Access the env variable
-    const res = await axios.post(
-      `${API_BASE_URL}/logout/`
-    );
+    const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
+
+    const res = await axios.post(`${API_BASE_URL}/logout/`);
     if (res.status === 200) {
       localStorage.removeItem("token");
+      localStorage.removeItem("user_info");
       navigate("/login");
-      // console.log("Logged out");
     }
   };
 
@@ -21,7 +97,7 @@ const Navbar = () => {
     <>
       <div className="app-header">
         <nav className="navbar navbar-expand-lg navbar-light">
-          {/* <ul className="navbar-nav">
+          <ul className="navbar-nav">
             <li className="nav-item d-block d-xl-none">
               <a
                 className="nav-link sidebartoggler nav-icon-hover"
@@ -31,22 +107,22 @@ const Navbar = () => {
                 <FontAwesomeIcon icon={faBars} />
               </a>
             </li>
-          </ul> */}
+          </ul>
+
           <div
             className="navbar-collapse justify-content-end px-0"
             id="navbarNav"
           >
             <ul className="navbar-nav flex-row ms-auto align-items-center justify-content-end">
               <li className="nav-item dropdown">
-                <a
-                  className="nav-link nav-icon-hover"
-                  href="#"
+                <p
+                  className="nav-link nav-icon-hover cursor-pointer"
                   id="drop2"
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
                 >
-                  SR
-                </a>
+                  {userEmail.slice(0, 1).toUpperCase()}
+                </p>
                 <div
                   className="dropdown-menu dropdown-menu-end dropdown-menu-animate-up"
                   aria-labelledby="drop2"
--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,28 +1,68 @@
+// import { toast } from "react-toastify";
+
+// export const checkAccessToken = () => {
+//     const accessToken = localStorage.getItem("token");
+//     return accessToken ? true : false;
+//   };
+  
+//   export const getAccessToken = () => {
+//     const accessToken = localStorage.getItem("token");
+//     return accessToken;
+//   };
+  
+//   export const setAccessToken = (token) => {
+//     localStorage.setItem("token", token);
+//   };
+  
+//   export const errorToast = (message) => {
+//     return toast.error(message, {
+//       position: "top-right",
+//       autoClose: 5000,
+//       hideProgressBar: false,
+//       closeOnClick: true,
+//       pauseOnHover: true,
+//       draggable: true,
+//       progress: undefined,
+//       theme: "light",
+//     });
+//   };
+
+
 import { toast } from "react-toastify";
 
 export const checkAccessToken = () => {
-    const accessToken = localStorage.getItem("token");
-    return accessToken ? true : false;
-  };
-  
-  export const getAccessToken = () => {
-    const accessToken = localStorage.getItem("token");
-    return accessToken;
-  };
-  
-  export const setAccessToken = (token) => {
-    localStorage.setItem("token", token);
-  };
-  
-  export const errorToast = (message) => {
-    return toast.error(message, {
-      position: "top-right",
-      autoClose: 5000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-      theme: "light",
-    });
-  };
+  const accessToken = localStorage.getItem("token");
+  return accessToken ? true : false;
+};
+
+export const getAccessToken = () => {
+  const accessToken = localStorage.getItem("token");
+  return accessToken;
+};
+
+export const setAccessToken = (token) => {
+  localStorage.setItem("token", token);
+};
+
+export const  getLocalStorage = (key) => {
+  const value = localStorage.getItem(key);
+  return value ? JSON.parse(value) : null;
+};
+
+export const setLocalStorage = (key, value) => {
+  localStorage.setItem(key, JSON.stringify(value));
+};
+
+
+export const errorToast = (message) => {
+  return toast.error(message, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+  });
+};
